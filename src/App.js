2b16012a098436a8d011dd3277c9da2a43cc8d58
kso2749 <rokmctkd67@naver.com>
2021-07-14 18:55:39 +0900

    6장

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
 
 class App extends React.Component{
 
   state = {
     isLoading: true,
-    movies: [], // 영화 데이터들
+    movies : [],
   };
 
-  componentDidMount(){
-    //로딩 완료
-    setTimeout(() => {
-      this.setState({isLoading: false});
-    }, 6000);
+  getMovies = async () =>{
+    const {data:{data:{movies},},} = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=raiting');
+    this.setState({movies, isLoading : false});
   }
+  
+  componentDidMount(){
+    this.getMovies();
+  }
+
   render() {    
-    const { isLoading } = this.state;
-    return <div>{isLoading ? 'Loading...' : 'We are ready!'}</div>;
+    const { isLoading, movies } = this.state;
+    return <section class="container">
+        {isLoading ? (<div class="loader"> <span class="loader__text">'Loading...'</span> </div> )
+          : (
+            <div class="movies">{movies.map(movie => (<Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />))}</div>)}
+      </section>;
   }
 }
 
